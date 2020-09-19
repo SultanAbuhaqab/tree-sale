994e807fc24bdc21f6bca09ba17be5313b82aff5
Sultan Abuhaqab <sabuh008@fiu.edu>
2020-09-19 19:34:49 +0300

    Added the necessary form components

--- NurseryTreesSale/NurseryTreesSale/frmTreeSale.Designer.vb
+++ NurseryTreesSale/NurseryTreesSale/frmTreeSale.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmTreeSale
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,291 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.radKeyLime = New System.Windows.Forms.RadioButton()
+        Me.radPersianLime = New System.Windows.Forms.RadioButton()
+        Me.radMeyerLemon = New System.Windows.Forms.RadioButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNumberOfTrees = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.chkDelivery = New System.Windows.Forms.CheckBox()
+        Me.chkPlanting = New System.Windows.Forms.CheckBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnSummary = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkGreen
+        Me.Label1.Location = New System.Drawing.Point(52, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(509, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nursery Tree Sale"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.txtNumberOfTrees)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(52, 73)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(509, 295)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Order Details"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.radMeyerLemon)
+        Me.GroupBox2.Controls.Add(Me.radPersianLime)
+        Me.GroupBox2.Controls.Add(Me.radKeyLime)
+        Me.GroupBox2.Location = New System.Drawing.Point(19, 20)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(209, 100)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Tree Type"
+        '
+        'radKeyLime
+        '
+        Me.radKeyLime.AutoSize = True
+        Me.radKeyLime.Location = New System.Drawing.Point(7, 20)
+        Me.radKeyLime.Name = "radKeyLime"
+        Me.radKeyLime.Size = New System.Drawing.Size(68, 17)
+        Me.radKeyLime.TabIndex = 0
+        Me.radKeyLime.TabStop = True
+        Me.radKeyLime.Text = "Key Lime"
+        Me.radKeyLime.UseVisualStyleBackColor = True
+        '
+        'radPersianLime
+        '
+        Me.radPersianLime.AutoSize = True
+        Me.radPersianLime.Location = New System.Drawing.Point(6, 43)
+        Me.radPersianLime.Name = "radPersianLime"
+        Me.radPersianLime.Size = New System.Drawing.Size(85, 17)
+        Me.radPersianLime.TabIndex = 1
+        Me.radPersianLime.TabStop = True
+        Me.radPersianLime.Text = "Persian Lime"
+        Me.radPersianLime.UseVisualStyleBackColor = True
+        '
+        'radMeyerLemon
+        '
+        Me.radMeyerLemon.AutoSize = True
+        Me.radMeyerLemon.Location = New System.Drawing.Point(7, 66)
+        Me.radMeyerLemon.Name = "radMeyerLemon"
+        Me.radMeyerLemon.Size = New System.Drawing.Size(89, 17)
+        Me.radMeyerLemon.TabIndex = 2
+        Me.radMeyerLemon.TabStop = True
+        Me.radMeyerLemon.Text = "Meyer Lemon"
+        Me.radMeyerLemon.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(260, 20)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(212, 256)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Number of Trees"
+        '
+        'txtNumberOfTrees
+        '
+        Me.txtNumberOfTrees.Location = New System.Drawing.Point(22, 167)
+        Me.txtNumberOfTrees.Name = "txtNumberOfTrees"
+        Me.txtNumberOfTrees.Size = New System.Drawing.Size(206, 20)
+        Me.txtNumberOfTrees.TabIndex = 3
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.chkPlanting)
+        Me.GroupBox3.Controls.Add(Me.chkDelivery)
+        Me.GroupBox3.Location = New System.Drawing.Point(22, 206)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(206, 70)
+        Me.GroupBox3.TabIndex = 4
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Additional Services"
+        '
+        'chkDelivery
+        '
+        Me.chkDelivery.AutoSize = True
+        Me.chkDelivery.Location = New System.Drawing.Point(7, 20)
+        Me.chkDelivery.Name = "chkDelivery"
+        Me.chkDelivery.Size = New System.Drawing.Size(64, 17)
+        Me.chkDelivery.TabIndex = 0
+        Me.chkDelivery.Text = "Delivery"
+        Me.chkDelivery.UseVisualStyleBackColor = True
+        '
+        'chkPlanting
+        '
+        Me.chkPlanting.AutoSize = True
+        Me.chkPlanting.Location = New System.Drawing.Point(7, 44)
+        Me.chkPlanting.Name = "chkPlanting"
+        Me.chkPlanting.Size = New System.Drawing.Size(64, 17)
+        Me.chkPlanting.TabIndex = 1
+        Me.chkPlanting.Text = "Planting"
+        Me.chkPlanting.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label5)
+        Me.GroupBox4.Controls.Add(Me.Label4)
+        Me.GroupBox4.Controls.Add(Me.TextBox2)
+        Me.GroupBox4.Controls.Add(Me.TextBox1)
+        Me.GroupBox4.Controls.Add(Me.txtName)
+        Me.GroupBox4.Controls.Add(Me.Label3)
+        Me.GroupBox4.Location = New System.Drawing.Point(52, 389)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(509, 128)
+        Me.GroupBox4.TabIndex = 2
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Customer Details"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(70, 31)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(400, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(72, 65)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(400, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(70, 99)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(400, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(19, 102)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Address"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(19, 68)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Phone"
+        '
+        'btnSummary
+        '
+        Me.btnSummary.BackColor = System.Drawing.Color.DarkGreen
+        Me.btnSummary.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSummary.ForeColor = System.Drawing.Color.White
+        Me.btnSummary.Location = New System.Drawing.Point(52, 544)
+        Me.btnSummary.Name = "btnSummary"
+        Me.btnSummary.Size = New System.Drawing.Size(126, 23)
+        Me.btnSummary.TabIndex = 3
+        Me.btnSummary.Text = "View Cost Summary"
+        Me.btnSummary.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Crimson
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(198, 544)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(126, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'frmTreeSale
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(603, 606)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSummary)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmTreeSale"
+        Me.Text = "Florida Exotic Citrus Nursery Tree Sale"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents chkPlanting As CheckBox
+    Friend WithEvents chkDelivery As CheckBox
+    Friend WithEvents txtNumberOfTrees As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents radMeyerLemon As RadioButton
+    Friend WithEvents radPersianLime As RadioButton
+    Friend WithEvents radKeyLime As RadioButton
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSummary As Button
+    Friend WithEvents btnExit As Button
 End Class
